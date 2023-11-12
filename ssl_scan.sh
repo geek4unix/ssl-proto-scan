49931373f342edbcf3968075eca19151b3f70318
Neil M <geek4unix@gmail.com>
2023-11-12 09:29:53 +0000

    Add files via upload
    updated detection and debug

--- ssl_scan.sh
+++ ssl_scan.sh
@@ -11,7 +11,11 @@ RESET=$(tput setaf 7 && tput setab 0)
 
 TLS_OPTIONS=$(2>&1 openssl s_client --help | egrep -i 'tls[0-9]|ssl.*[0-9]'|sed 's/-//g')
 USAGE="
-Enter a host and port number and timeout as arguments e.g. $GREEN $0 $(hostname -f) 443 5$RESET
+Enter a host and port number and timeout as arguments e.g. $GREEN $0 $(hostname -f) 443 5$RESET 
+
+Or with debug:
+
+Enter a host and port number and timeout as arguments e.g. $GREEN $0 $(hostname -f) 443 5$RESET DEBUG
 
 Current PROTOS: $PROTOS
 
@@ -23,6 +27,11 @@ and set them to any combination of:
 
 $TLS_OPTIONS"
 
+if [[ $4 == "DEBUG" ]]; then
+  DEBUG=1
+else
+  DEBUG=0
+fi
 
 if [ -z $OPENSSL ]; then
   echo "Make sure openssl is installed, or set the path to openssl version in your PATH"
@@ -38,15 +47,18 @@ sc=0; fc=0
 
 for proto in $PROTOS
 do
-  if [[ $DEBUG -eq 0 ]];then
-    2>&1>/dev/null timeout $3 2>&1>/dev/null openssl s_client -connect $1:$2 -$proto
-    RET=$?
-  else
-    timeout $3 openssl s_client -connect $1:$2 -$proto
-    RET=$?
-  fi
+  OUTPUT=$(2>&1 timeout $3 2>&1 2>&1 openssl s_client -connect $1:$2 -$proto)
+  RET=$?
 
-  if [ $RET -eq 0 ] || [ $RET -eq 124 ] ; then
+  # must have cert data ! Make sure there is a Cipher
+  CC=$(echo -n $OUTPUT|grep "Server certificate" | wc -l)
+  PU=$(echo -n $OUTPUT|grep "Protocol.*:")
+  PUL=$(echo -n $OUTPUT|grep "Protocol.*:"|wc -l)
+  if [ $DEBUG -ne 0 ]; then
+    echo "OUTPUT=$OUTPUT"
+    echo "PROTO USED =$PU"
+  fi
+  if [ $PUL -gt 0 ]  ; then
     let sc=$sc+1
     echo $GREEN"Successfully$RESET connected to $1 on port $2 using $YELLOW$proto$RESET, return code: $RET"
   else
