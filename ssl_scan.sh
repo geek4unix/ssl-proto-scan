9a171eff40c2543d6ad1ab51b13c3b0e44e87d2a
Neil M <geek4unix@gmail.com>
2023-11-12 14:36:59 +0000

    Add files via upload
    More detection changes, added service output matched from /etc/services

--- ssl_scan.sh
+++ ssl_scan.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 OPENSSL=$(which openssl)
-DEBUG=0
-PROTOS="tls1 tls1_1 tls1_2 tls1_3 dtls1 dtls1_2 no_ssl3"
+PROTOS="tls1 tls1_1 tls1_2 tls1_3 no_ssl3"
 
 # Set up colours
 RED=$(tput setaf 1)
@@ -47,26 +46,30 @@ sc=0; fc=0
 
 for proto in $PROTOS
 do
-  OUTPUT=$(2>&1 timeout $3 2>&1 2>&1 openssl s_client -connect $1:$2 -$proto)
+  OUTPUT=$(2>&1 echo "Hello World" | timeout $3 2>&1 2>&1 openssl s_client -connect $1:$2 -$proto)
   RET=$?
+  SERVICE=$(grep ".* $2\/[tu]dp" /etc/services |awk {'print $1'})
 
   # must have cert data or a non NONE Cipher
   CC=$(echo -n $OUTPUT|grep "Server certificate" | wc -l)
-  PU=$(echo -n $OUTPUT|egrep "Protocol.*:|Cipher.*"|grep -v NONE)
-  PUL=$(echo -n $OUTPUT|egrep "Protocol.*:|Cipher.*"|grep -v NONE|wc -l)
+  WV=$(echo -n $OUTPUT|grep "SSL.*wrong version number" | wc -l)
+  SS=$(echo -n $OUTPUT|egrep -B2 "SSL-Session.*:|self-signed certificate"|tr -s " "|tr -d ':'|egrep -v "New, (NONE), Cipher is (NONE)"|grep -v "Cipher.*0000"|wc -l)
+  PU=$(echo -n $OUTPUT|egrep "Protocol.*:|Cipher.*"|grep -v New.*NONE)
+  PUL=$(echo -n $OUTPUT|egrep "Protocol.*:|Cipher.*"|grep -v New.*NONE|wc -l)
   if [ $DEBUG -ne 0 ]; then
+    echo "WV =$WV"
+    echo "SS =$SS"
     echo "PROTO USED =$PU"
     echo "-----
 OUTPUT=$OUTPUT
 -----"
   fi
-  if [[ $PUL -gt 0 ]] ||  [[ $CC -gt 0 ]] ; then
-
+  if ([[ $CC -gt 0 ]] || [[ $SS -gt 0 ]]) && ( [[ $WV -lt 1 ]] ) ; then
     let sc=$sc+1
-    echo $GREEN"Successfully$RESET connected to $1 on port $2 using $YELLOW$proto$RESET, return code: $RET"
+    echo $GREEN"Successfully$RESET connected to $1 on port $2 using $YELLOW$proto$RESET, service: $YELLOW$SERVICE$RESET - return code: $RET"
   else
     let fc=$fc+1
-    echo $RED"Failed$RESET to connect $1 on port $2 using $YELLOW$proto$RESET, return code: $RET"
+    echo $RED"Failed$RESET to connect $1 on port $2 using $YELLOW$proto$RESET, service: $YELLOW$SERVICE$RESET - return code: $RET"
   fi
   sleep 1
 done
